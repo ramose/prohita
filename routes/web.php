f7fba6f23030293d2c5d81ebd7d743e3a8f0e756
Leo <leopapilaya@gmail.com>
2023-02-08 08:01:09 +0800

    Add breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\clientController;
-// use DebugBar\DebugBar;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     // Debugbar::info("hello");
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/', [clientController::class, "home"]);
-Route::get('/client', [clientController::class, "index"]);
-Route::get('/client/create', [clientController::class, "create"]);
-Route::post('/client/store', [clientController::class, "store"]);
-Route::get('/client/{id}/edit', [clientController::class, "edit"]);
-Route::put('/client/{id}', [clientController::class, "update"]);
-Route::delete('/client/{id}', [clientController::class, "destroy"]);
+require __DIR__.'/auth.php';
